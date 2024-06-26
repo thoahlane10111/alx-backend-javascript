9175178755c200e596cd84a28e4015b7627ae206
thoahlane10111 <thoahlane10111@gmail.com>
2024-06-26 22:56:35 +0000

    new

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,22 +1,10 @@
-/*
- * Defines a new function that resolve all promises made by functions: uploadPhoto() and
- * createUser()
- */
-
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const listOfPromises = [ // List of promises
-    uploadPhoto(), // Get resolved data
-    createUser(),
-  ];
-
-  return Promise.all(listOfPromises)
-    .then((listOfResolved) => { // [ uploadPhotoResolveData, createUserResolvedData ]
-      const { body } = listOfResolved[0];
-      const { firstName, lastName } = listOfResolved[1];
-
-      console.log(body, firstName, lastName);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
