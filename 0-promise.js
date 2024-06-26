17120464adfa1668788bd6a6db5d68143958a41d
thoahlane10111 <thoahlane10111@gmail.com>
2024-06-26 18:51:18 +0000

    latest

--- 0-promise.js
+++ 0-promise.js
@@ -1,6 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // This is a simple promise that resolves immediately.
-    resolve('Success');
-  });
+  return new Promise(() => {});
 }
