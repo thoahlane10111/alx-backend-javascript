4ce7ae0f80768bde13b300cb02c20b014b7e1bb2
thoahlane10111 <thoahlane10111@gmail.com>
2024-06-26 22:15:08 +0000

    new

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,36 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    browser: false,
+    es6: true,
+    jest: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:jest/all',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['jest'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
+
--- utils.js
+++ utils.js
@@ -1,15 +1,14 @@
-function uploadPhoto() {
-  return {
+export function uploadPhoto() {
+  return Promise.resolve({
     status: 200,
     body: 'photo-profile-1',
-  };
+  });
 }
 
-function createUser() {
-  return {
+
+export function createUser() {
+  return Promise.resolve({
     firstName: 'Guillaume',
     lastName: 'Salva',
-  };
+  });
 }
-
-module.exports = { uploadPhoto, createUser };
